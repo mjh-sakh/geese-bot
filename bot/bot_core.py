00803ceaec84d7181351f3959453673a88f1ca27
mjh-sakh <madjahead@gmail.com>
2022-01-20 22:18:42 +1100

    add postgresql connection and log

--- bot/bot_core.py
+++ bot/bot_core.py
@@ -1,20 +1,26 @@
 """Core module to start and run bot."""
 
 import logging
+from datetime import datetime
+from functools import partial
 
+import psycopg2
 from telethon import TelegramClient, events
 
 from bot.config import BOT_CONFIG
 
 
 def run():
+    conn = psycopg2.connect("dbname=geese_bot user=goose")
     bot = TelegramClient(BOT_CONFIG.session, BOT_CONFIG.api_id, BOT_CONFIG.api_hash)
-    bot.on(events.NewMessage)(message_handler)
+    message_handler_with_db = partial(message_handler, conn)
+    bot.on(events.NewMessage)(message_handler_with_db)
     bot.start(bot_token=BOT_CONFIG.bot_token)
     bot.run_until_disconnected()
+    conn.close()
 
 
-async def message_handler(event):
+async def message_handler(db_conn, event):
     # chat = await event.get_chat()
     # await bot.download_profile_photo(sender)
     chat_id = event.chat_id
@@ -23,5 +29,15 @@ async def message_handler(event):
     logging.info(f'incoming message: {text}')
     logging.info(f'chat_id: {chat_id}')
     logging.info(f'sender_id: {sender_id}')
+
+    cur = db_conn.cursor()
+    cur.execute("""
+                INSERT INTO messages (sender_id, chat_id, message, date) 
+                VALUES (%s, %s, %s, %s)
+                """,
+                (sender_id, chat_id, text, datetime.now()))
+    cur.close()
+    db_conn.commit()
+
     if text.lower() == 'hello':
         await event.reply('Hi!')
